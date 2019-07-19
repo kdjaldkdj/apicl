7164c17901ba76c0e0773099fb46eab4e161bc1e
Sergio Galvan <sergalma@gmail.com>
2019-07-19 08:16:54 +0200

    cmd(version): use ldflags to set version on CLI

--- cmd/apicl/main.go
+++ cmd/apicl/main.go
@@ -10,13 +10,22 @@ import (
 
 const usage = "Manage apiary"
 
+var version = "0.1.0"
+
+var GitHash string
+var GitTag string
+
 func main() {
 
 	app := cli.NewApp()
 	app.Name = "apicl"
 	app.Usage = usage
 	app.Commands = apicl.Commands()
-	app.Version = "0.1.0"
+
+	if GitHash != "" || GitTag != "" {
+		version = fmt.Sprintf("%s-%s", GitHash, GitTag)
+	}
+	app.Version = version
 	if !apicl.CheckConfigEnv() {
 		fmt.Fprintf(os.Stderr, "Please, configure your APIARY_API_KEY before using this tool.\n")
 		os.Exit(1)
