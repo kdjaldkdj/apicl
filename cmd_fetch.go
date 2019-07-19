200c5d42e553f366449b15a55293eb70c33fc714
Sergio Galvan <sergalma@gmail.com>
2019-07-19 09:06:47 +0200

    cmd(hack): dirty hack to avoid passing empty api-names to fetch

--- cmd_fetch.go
+++ cmd_fetch.go
@@ -2,12 +2,17 @@ package apicl
 
 import (
 	"fmt"
+	"os"
 
 	"gopkg.in/urfave/cli.v1"
 )
 
 func fetch(l *cli.Context) error {
 	apiName := l.String("api-name")
+	if apiName == "" {
+		fmt.Println("--api-name must be provided. Please use --help")
+		os.Exit(1)
+	}
 	r, err := c.Fetch(apiName)
 	if err != nil {
 		return err
